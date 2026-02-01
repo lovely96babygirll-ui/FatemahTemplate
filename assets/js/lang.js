fe5c30f24be9b94173b18a8574b1f587c1e64c8b
mohammad kheder <lovely96babygirll@gmail.com>
2026-02-01 06:03:11 +0200

    nav bar

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,45 +1,8 @@
 $(document).ready(function () {
   const lang = getCookie('lang') || 'en';
-  setLanguage(lang);
+  setLanguage(lang, false); 
 });
 
-$(document).on('click', '.lang-btn', function () {
-  const lang = $(this).data('lang');
-  setLanguage(lang);
-});
-
-function setLanguage(lang) {
-  setCookie('lang', lang, 365);
-  loadLanguageFile(lang);
-  updateDirection(lang);
-}
-
-function loadLanguageFile(lang) {
-  $.getJSON('lang/' + lang + '.json', function (data) {
-
-    $('[data-i18n]').each(function () {
-      const key = $(this).data('i18n');
-      if (data[key]) {
-        $(this).text(data[key]);
-      }
-    });
-
-  }).fail(function () {
-    console.error('Language file not found');
-  });
-}
-
-
-function updateDirection(lang) {
-  if (lang === 'ar') {
-    $('html').attr('dir', 'rtl').attr('lang', 'ar');
-    $('body').addClass('rtl');
-  } else {
-    $('html').attr('dir', 'ltr').attr('lang', 'en');
-    $('body').removeClass('rtl');
-  }
-}
-// ...
 function setCookie(name, value, days) {
   const d = new Date();
   d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
@@ -57,5 +20,86 @@ function getCookie(name) {
     }
   }
   return "";
+}
+$(document).on('click', '.lang-btn', function () {
+  const lang = $(this).data('lang');
+  setLanguage(lang);
+});
+
+function setLanguage(lang, writeCookie = true) {
+
+  if (writeCookie) {
+    setCookie('lang', lang, 365);
+  }
+
+  loadLanguageFile(lang);
+  updateDirection(lang);
+  updateLangDropdown(lang);
+}
+
+
+
+function loadLanguageFile(lang) {
+  $.getJSON('lang/' + lang + '.json', function (data) {
+
+    $('[data-i18n]').each(function () {
+      const key = $(this).data('i18n');
+      if (data[key]) {
+        $(this).text(data[key]);
+      }
+    });
+
+     const pageKey = $('body').data('page');
+
+    if (pageKey && data[pageKey + '.title']) {
+      document.title = data[pageKey + '.title'];
+    }
+
+  }).fail(function () {
+    console.error('Language file not found');
+  });
+  
+}
+
+
+function updateDirection(lang) {
+  if (lang === 'ar') {
+    $('html').attr('dir', 'rtl').attr('lang', 'ar');
+    $('body').addClass('rtl');
+  } else {
+    $('html').attr('dir', 'ltr').attr('lang', 'en');
+    $('body').removeClass('rtl');
+  }
+}
+function updateLangDropdown(lang){
+
+  let otherLang = lang === "en" ? "ar" : "en";
+
+  const langData = {
+    en: {
+      text: "EN",
+      flag: "assets/img/flags/gb.svg"
+    },
+    ar: {
+      text: "AR",
+      flag: "assets/img/flags/sa.svg"
+    }
+  };
+
+  $("#activeLang").text(langData[lang].text);
+  $("#activeLangFlag").attr("src", langData[lang].flag);
+
+  $("#langDropdownList").html(`
+    <li>
+      <a class="dropdown-item lang-btn d-flex align-items-center gap-2"
+         data-lang="${otherLang}">
+
+         <img src="${langData[otherLang].flag}" width="20">
+         ${langData[otherLang].text}
+
+      </a>
+    </li>
+  `);
+
 }
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -14,35 +14,36 @@ window.APP = {
         ? '/' + window.location.pathname.split('/')[1]
         : ''
 };
+$("#year").text(new Date().getFullYear());
 
-function fixNavLinks() {
-    $(".nav-dynamic").each(function () {
-        let href = $(this).attr("href");
+// function fixNavLinks() {
+//     $(".nav-dynamic").each(function () {
+//         let href = $(this).attr("href");
 
-        if (!href.startsWith("http")) {
-            $(this).attr("href", APP.BASE_PATH + href);
-        }
-    });
-}
+//         if (!href.startsWith("http")) {
+//             $(this).attr("href", APP.BASE_PATH + href);
+//         }
+//     });
+// }
 
-$(function () {
+// $(function () {
 
-    let lang = getLang() || "en";
-    let base = "/" + lang + "/";
+//     let lang = getLang() || "en";
+//     let base = "/" + lang + "/";
 
-    // navbar links
-    $("[data-link]").each(function () {
-        let page = $(this).data("link");
-        $(this).attr("href", base + page + ".html");
-    });
+//     // navbar links
+//     $("[data-link]").each(function () {
+//         let page = $(this).data("link");
+//         $(this).attr("href", base + page + ".html");
+//     });
 
-});
+// });
 
-function switchLang(lang) {
-    setLang(lang);
-    let page = window.location.pathname.split("/").pop();
-    window.location.href = "/" + lang + "/" + page;
-}
+// function switchLang(lang) {
+//     setLang(lang);
+//     let page = window.location.pathname.split("/").pop();
+//     window.location.href = "/" + lang + "/" + page;
+// }
 // ...................................
 $(window).on("scroll", function () {
     $(".animate").each(function () {
@@ -54,14 +55,44 @@ $(window).on("scroll", function () {
     });
 });
 //................................
-$(document).ready(function () {
-  const currentPage = window.location.pathname.split('/').pop();
+// $(document).ready(function () {
+//   const currentPage = window.location.pathname.split('/').pop();
 
-  $('.navbar .nav-link').each(function () {
-    const linkPage = $(this).attr('href');
+//   $('.navbar .nav-link').each(function () {
+//     const linkPage = $(this).attr('href');
 
-    if (linkPage === currentPage) {
-      $(this).addClass('active');
+//     if (linkPage === currentPage) {
+//       $(this).addClass('active');
+//     }
+//   });
+// });
+function setActiveNav() {
+  const currentPage = window.location.pathname
+    .split("/")
+    .pop()
+    .replace(".html", "") || "index";
+
+  $(".nav-link").removeClass("active");
+
+  $(`.nav-link[data-page="${currentPage}"]`).addClass("active");
+}
+function setActiveNavLink() {
+  let currentPage = window.location.pathname.split('/').pop();
+console.log(currentPage);
+  // handle root "/"
+  if (currentPage === '' || currentPage === '/' || currentPage === '#') {
+    currentPage = 'index.html';
+  }
+
+  currentPage = currentPage.replace('.html', '');
+console.log("fff",currentPage);
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.classList.remove('active');
+
+    if (link.dataset.page === currentPage) {
+       console.log("fbff",currentPage); 
+      link.classList.add('active');
     }
   });
-});
+}
+
